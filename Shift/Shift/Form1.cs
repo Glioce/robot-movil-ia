fcd9e170d79d0cf696b2f48ac7706c2ef29b0f42
Glioce <0>
2020-03-10 17:38:49 -0600

    Probado envio de datos por serial

---
+++ Shift/Shift/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports; //para usar serial
+using System.Threading;
+
+namespace Shift
+{
+    public partial class Form1 : Form
+    {
+        // Variables de la clase Form1
+        static bool _continue; //auxiliar
+        static SerialPort _serialPort; //objeto serial
+        string name; //
+        string message; //
+        StringComparer stringComparer = StringComparer.OrdinalIgnoreCase;
+        Thread readThread = new Thread(Read);
+
+        // Create a new SerialPort object with default settings.
+        //_serialPort = new SerialPort();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Inicializar puerto serial
+            _serialPort = new SerialPort();
+            _serialPort.PortName = "COM7";
+            _serialPort.BaudRate = 9600;
+            _serialPort.Parity = Parity.None;
+            _serialPort.DataBits = 8;
+            _serialPort.StopBits = StopBits.One;
+            _serialPort.Handshake = Handshake.None;
+
+            // Set the read/write timeouts
+            _serialPort.ReadTimeout = 500;
+            _serialPort.WriteTimeout = 500;
+
+            _serialPort.Open();
+            _continue = true;
+            readThread.Start();
+
+            Console.Write("Name: ");
+            name = Console.ReadLine();
+
+            Console.WriteLine("Type QUIT to exit");
+        }
+
+        public static void Read()
+        {
+            while (_continue)
+            {
+                try
+                {
+                    string message = _serialPort.ReadLine();
+                    Console.WriteLine(message);
+                }
+                catch (TimeoutException) { }
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            String s1 = "";
+            String s2 = "";
+            if (e.Shift)
+            {
+                s1 = "Shift + ";
+            }
+            if (e.KeyCode == Keys.Up)
+            {
+                s2 = "Flecha arriba";
+                _serialPort.Write("A");
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                s2 = "Flecha abajo";
+                _serialPort.Write("B");
+            }
+            if (e.KeyCode == Keys.Left)
+            {
+                s2 = "Flecha izquierda";
+                _serialPort.Write("C");
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                s2 = "Flecha derecha";
+                _serialPort.Write("D");
+            }
+            label1.Text = s1 + s2;
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            label1.Text = "Presiona una flecha,  o una flecha + shift";
+        }
+       
+    }
+}
--- tres_en_uno_v0/tres_en_uno_v0.ino
+++ tres_en_uno_v0/tres_en_uno_v0.ino
@@ -6,10 +6,12 @@
 #define PIN_E2 13 //encoder 2
 #define PIN_M1 4  //motor 1
 #define PIN_M2 3  //motor 2
+#define PIN_SHARP A0
+#define PIN_SERVO A1
 
 // crear objeto para el sensor Sharp
 #include "Sharp.h"
-Sharp sharp1(A0);
+Sharp sharp1(PIN_SHARP);
 
 // crear objetos para encoders
 #include "Encoder.h"
@@ -18,62 +20,74 @@ Encoder encoder2(PIN_E2);
 
 // crear objeto para el servo
 #include <Servo.h>
-Servo myservo; 
+Servo myservo;
 
+// Variables globales
 int pos = 0; // posición del servo
 int incre = 1; //incremento de posición del servo
+
 void setup() {
-  // put your setup code here, to run once:
-pinMode (PIN_E1, INPUT);
-pinMode (PIN_E2, INPUT);
+  // entradas para encoders
+  pinMode(PIN_E1, INPUT);
+  pinMode(PIN_E2, INPUT);
 
-pinMode (PIN_M1, OUTPUT);
-pinMode (PIN_M2, OUTPUT);
+  // salidas para motores
+  pinMode(PIN_M1, OUTPUT);
+  pinMode(PIN_M2, OUTPUT);
 
+  // motores apagados
+  analogWrite(PIN_M1, 0);
+  analogWrite(PIN_M2, 0);
 
+  myservo.attach(PIN_SERVO);
 
-analogWrite(PIN_M1, 255);
-analogWrite(PIN_M2, 255);
-
-Serial.begin(9600);
-
-myservo.attach(A1);  // attaches the servo on pin 9 to the servo object
-
+  // iniciar comunicación serial
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  while (Serial.available() > 0) {
+    byte comando = Serial.read();
 
-if(encoder1.pulso()){
-  Serial.println(encoder1.e);
-  delay(2);  
-}
-if(encoder2.pulso()){
-  Serial.println(encoder2.e);
-  delay(2);  
-}
-
-Serial.println(sharp1.distancia(20));
-delay(10);
-
-
-
-      pos += incre;
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);
-   if (pos == 180){
-                          // waits 15ms for the servo to reach the position
-
-     incre=-1;
+    switch (comando) {
+      case 'A': //avanzar
+        analogWrite(PIN_M1, 255);
+        analogWrite(PIN_M2, 255);
+        break;
+      case 'B': //detener
+        analogWrite(PIN_M1, 0);
+        analogWrite(PIN_M2, 0);
+        break;
+      case 'C': //izquierda
+        analogWrite(PIN_M1, 0);
+        analogWrite(PIN_M2, 255);
+        break;
+      case 'D': //derecha
+        analogWrite(PIN_M1, 255);
+        analogWrite(PIN_M2, 0);
+        break;
+    }
   }
-   if (pos == 0){
-                          // waits 15ms for the servo to reach the position
 
-     incre=1;
+  if (encoder1.pulso()) {
+    Serial.println(encoder1.e);
+    delay(2);
   }
-  
+  if (encoder2.pulso()) {
+    Serial.println(encoder2.e);
+    delay(2);
+  }
 
+  Serial.println(sharp1.distancia(20));
 
+  pos += incre;
+  myservo.write(pos);
+  if (pos == 180) {
+    incre = -1;
+  }
+  if (pos == 0) {
+    incre = 1;
+  }
 
-
+  delay(10);
 }
